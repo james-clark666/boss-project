f80cda8a309fa68e7100c09ecc40dd0b65a24bb8
james-clark666 <695625965@qq.com>
2022-04-06 15:26:20 +0800

    init

---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login">用户登录</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import request from '@/utils/request'
+export default Vue.extend({
+  name: 'LoginIndex',
+  data () {
+    return {}
+  },
+  methods: {
+    async onSubmit () {
+      const { data } = await request({
+        method: 'POST',
+        url: '/front/user/login'
+      })
+      console.log(data)
+    }
+
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
